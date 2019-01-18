804372f582e49dfcd685e037f1294a9c2ca0dc5e
rellonjessrel <35474756+rellonjessrel@users.noreply.github.com>
2019-01-19 00:30:31 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Activity Diagram</title>
+</head>
+<body>
+<img src="activity.jpg">
+<p> This activity diagram is showing the certain flow of a lottery online how to bet and the time of draw every games so that</p> <p>
+peolpe well be aware about the games </p>
+</body>
+</html>
